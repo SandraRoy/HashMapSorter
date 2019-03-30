ed741138e6d35a7044838af3ed6d0857a115a9b3
Sandra Roy <Sandra.D.Roy-1@ou.edu>
2019-03-29 22:53:59 -0500

    first commit

---
+++ MesoAscii.java
@@ -0,0 +1,20 @@
+public class MesoAscii extends MesoAsciiAbstract 
+{ 
+
+	MesoStation StID;
+	public MesoAscii ( MesoStation id)//takes MesoStation object as parameter
+	{
+		StID=	id;
+	}
+	public int calAverage()
+	{  
+
+		double sum=0, average=0;
+		for (int i=0;i<4;i++)
+		{
+			char ch=StID.getStID().charAt(i); 
+			sum=sum+(int)(ch);
+		}
+		return (int)(Math.round(sum/4.0));
+	}
+}
